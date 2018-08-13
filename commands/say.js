90afdff97be59567cdb5aba921bd5a2669683d8b
Hansel Wei <darkmastermindz@gmail.com>
2018-08-13 02:38:45 -0400

    Updated and cleaned up code

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.delete();
+  message.delete().catch(O_o=>{});
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
   let botmessage = args.join(" ");
   message.channel.send(botmessage);
